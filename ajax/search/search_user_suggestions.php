7e69f13a0d0c5743232177359f2eadc814fd89d8
brentspine <55391576+brentspine@users.noreply.github.com>
2022-12-21 18:17:12 +0100

    Fix Issue #3 #4 #5 #6

--- ajax/search/search_user_suggestions.php
+++ ajax/search/search_user_suggestions.php
@@ -17,21 +17,21 @@ if(!isset_any_params($_GET, "query")) {
 $query = $_GET["query"];
 
 $sql = '
-(SELECT * FROM (SELECT CONCAT(CONCAT(first_name, " "), last_name) AS full_name, id, class FROM users) AS result WHERE full_name LIKE ?) UNION
-(SELECT * FROM (SELECT CONCAT(CONCAT(first_name, " "), last_name) AS full_name, id, class FROM users) AS result WHERE full_name LIKE CONCAT(?, "%")) UNION
-(SELECT CONCAT(CONCAT(first_name, " "), last_name), id, class AS full_name FROM users WHERE first_name LIKE ? OR last_name LIKE ?) UNION
-(SELECT CONCAT(CONCAT(first_name, " "), last_name), id, class AS full_name FROM users WHERE first_name LIKE CONCAT(?, "%") OR last_name LIKE CONCAT(?, "%")) UNION
-(SELECT CONCAT(CONCAT(first_name, " "), last_name), id, class AS full_name FROM users WHERE first_name LIKE CONCAT(CONCAT("%", ?), "%") OR last_name LIKE CONCAT(CONCAT("%", ?), "%")) LIMIT 15;';
+(SELECT * FROM (SELECT CONCAT(CONCAT(first_name, " "), last_name) AS full_name, id FROM users) AS result WHERE full_name LIKE ?) UNION
+(SELECT * FROM (SELECT CONCAT(CONCAT(first_name, " "), last_name) AS full_name, id FROM users) AS result WHERE full_name LIKE CONCAT(?, "%")) UNION
+(SELECT CONCAT(CONCAT(first_name, " "), last_name), id AS full_name FROM users WHERE first_name LIKE ? OR last_name LIKE ?) UNION
+(SELECT CONCAT(CONCAT(first_name, " "), last_name), id AS full_name FROM users WHERE first_name LIKE CONCAT(?, "%") OR last_name LIKE CONCAT(?, "%")) UNION
+(SELECT CONCAT(CONCAT(first_name, " "), last_name), id AS full_name FROM users WHERE first_name LIKE CONCAT(CONCAT("%", ?), "%") OR last_name LIKE CONCAT(CONCAT("%", ?), "%")) LIMIT 15;';
 $result = prepared_statement_result($sql, $con, true, "ssssssss", $query, $query, $query, $query, $query, $query, $query, $query);
 
-$users =  get_multiple_database_entries_result($result, array("id", "full_name", "class"));
+$users =  get_multiple_database_entries_result($result, array("id", "full_name"));
 
 $response = [];
 foreach($users as $c) {
+    if($c[0] == $uid || strlen($c[1]) <= 2) continue;
     $user = [];
     $user["id"] = $c[0];
     $user["full_name"] = $c[1];
-    $user["class"] = $c[2];
     $response[] = $user;
 }
 echo json_encode($response);
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-echo '<h1>404 not found</h1>';
+include $_SERVER["DOCUMENT_ROOT"] . "/include/functions.php";
+
+if(strlen(get_current_path()) <= 1) {
+    echo '<script>window.location.href = "/login"</script>';
+    return;
+}
+echo '<h1>404 Not Found</h1>';
 
 ?>
